bddc90b539086498516e06f505abc31255af92d3
Jesse Gador <jessegador@gmail.com>
2024-08-08 13:17:34 +0800

    Add custom properties in login & registration

--- src/AKS.Demo/Migrations/20240808050055_UpdateUser.Designer.cs
+++ src/AKS.Demo/Migrations/20240808050055_UpdateUser.Designer.cs
@@ -4,6 +4,7 @@ using AKS.Demo.Areas.Identity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AKS.Demo.Migrations
 {
     [DbContext(typeof(AksDemoContext))]
-    partial class AksDemoContextModelSnapshot : ModelSnapshot
+    [Migration("20240808050055_UpdateUser")]
+    partial class UpdateUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
