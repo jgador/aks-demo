be222b3052de70ca7240bc6510a95b20c73360aa
Jesse Gador <jessegador@gmail.com>
2024-08-08 12:59:43 +0800

    Add scaffold for Identity

--- src/AKS.Demo/Areas/Identity/Data/ApplicationUser.cs
+++ src/AKS.Demo/Areas/Identity/Data/ApplicationUser.cs
@@ -1,13 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AKS.Demo.Areas.Identity.Data;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    public ApplicationUser()
+    {
+    }
+
+    [Required]
+    [StringLength(500)]
+    [Column("FirstName")]
+    public string FirstName { get; set; }
+
+    [Required]
+    [StringLength(500)]
+    [Column("LastName")]
+    public string LastName { get; set; }
 }
 
--- src/AKS.Demo/Program.cs
+++ src/AKS.Demo/Program.cs
@@ -4,63 +4,63 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AKS.Demo
 {
-	public class Program
-	{
-		public static async Task Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
-			ConfigureServices(builder);
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            ConfigureServices(builder);
 
-			builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-			builder.Services.AddRazorPages();
+            builder.Services.AddRazorPages();
 
-			var app = builder.Build();
+            var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseMigrationsEndPoint();
-			}
-			else
-			{
-				app.UseExceptionHandler("/Error");
-			}
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
 
-			app.UseStaticFiles();
-			app.UseCors();
-			app.UseRouting();
-			app.UseAuthentication();
-			app.UseAuthorization();
+            app.UseStaticFiles();
+            app.UseCors();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
-			app.MapRazorPages();
+            app.MapRazorPages();
 
-			await app.RunAsync().ConfigureAwait(false);
-		}
+            await app.RunAsync().ConfigureAwait(false);
+        }
 
-		private static void ConfigureServices(WebApplicationBuilder builder)
-		{
-			AddAksDemoContext(builder.Services, builder.Configuration);
+        private static void ConfigureServices(WebApplicationBuilder builder)
+        {
+            AddAksDemoContext(builder.Services, builder.Configuration);
 
-			builder.Services
-				.AddDefaultIdentity<ApplicationUser>(options =>
-				{
-					options.SignIn.RequireConfirmedAccount = true;
-					options.Password.RequireDigit = false;
-					options.Password.RequireLowercase = false;
-				})
-				.AddEntityFrameworkStores<AksDemoContext>();
+            builder.Services
+                .AddDefaultIdentity<ApplicationUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                })
+                .AddEntityFrameworkStores<AksDemoContext>();
 
-			builder.Services.AddCors(o => o.AddDefaultPolicy(c => c.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+            builder.Services.AddCors(o => o.AddDefaultPolicy(c => c.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
 
-			builder.Services.AddControllersWithViews();
-		}
+            builder.Services.AddControllersWithViews();
+        }
 
-		private static void AddAksDemoContext(IServiceCollection services, IConfiguration configuration)
-		{
-			var contextOptions = configuration.GetSection(AksDemoContextOptions.SectionName).Get<AksDemoContextOptions>();
+        private static void AddAksDemoContext(IServiceCollection services, IConfiguration configuration)
+        {
+            var contextOptions = configuration.GetSection(AksDemoContextOptions.SectionName).Get<AksDemoContextOptions>();
 
-			services.AddDbContext<AksDemoContext>(options => options.UseSqlServer(contextOptions.ConnectionString));
-		}
-	}
+            services.AddDbContext<AksDemoContext>(options => options.UseSqlServer(contextOptions.ConnectionString));
+        }
+    }
 }
